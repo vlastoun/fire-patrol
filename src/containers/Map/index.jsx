6b823c32f599dd7a7cc950d67b4a75d98690d47d
Vlastik <vlastoun@gmail.com>
2017-08-23 19:41:41 +0200

    map change

--- src/containers/Map/index.jsx
+++ src/containers/Map/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Map from './operativní karty 2016.png'
+import styled from 'styled-components';
+import Map from './operativni-karty-mapa.png';
+
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable no-console */
+const Img = styled.img`
+  overflow-x: hidden;
+  overflow-y: hidden;
+  max-width: 100%;
+`;
 
 class MainMap extends React.Component {
   render() {
     return (
       <div>
-        <img src={Map} alt='mapa'/>
+        <Img src={Map} alt="mapa" />
       </div>
     );
   }
