747f8e40562fd56faed00a4246fd3aab75858f6c
Vlastik <vlastoun@gmail.com>
2017-08-26 10:53:41 +0200

    buildings page route change

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -5,12 +5,14 @@ import MainMenu from './MainMenu';
 import HydrantsMap from '../../components/Map-hydrants';
 import LaddersMap from '../../components/Map-ladders';
 
+import BuildingPage from '../BuildingPage';
+
 const App = () => (
   <div>
     <MainMenu />
     <Switch>
       <Route exact path="/" component={MapPage} />
-      <Route exact path="/building/:id" component={MapPage} />
+      <Route exact path="/buildings/:id" component={BuildingPage} />
       {/* TODO: nest hydrants and ladders under 
       layer of Points component - must have maps with correct width
       Just move those subroutes under another router in MapPage */}
