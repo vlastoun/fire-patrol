b1592fe9bcfe17cae8f2a1036b7b0d58ae646310
Vlastik <vlastoun@gmail.com>
2017-08-26 09:42:34 +0200

    routes

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from '../HomePage';
+import MapPage from '../MapPage';
 import MainMenu from './MainMenu';
 
 const App = () => (
   <div>
     <MainMenu />
     <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/:map/:id" component={HomePage} />
-      <Route component={HomePage} />
+      <Route exact path="/" component={MapPage} />
+      <Route exact path="/:map/:id" component={MapPage} />
+      <Route component={MapPage} />
     </Switch>
   </div>
 );
--- src/containers/MapPage/index.jsx
+++ src/containers/MapPage/index.jsx
@@ -2,31 +2,20 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Map from '../Map';
 import Point from '../../components/Point';
 import {group, objects} from '../../data';
 
-const Header = styled.h2`
-  background-color: #00ff00;
-`;
-
-const mockFnc = () => console.log('Not implemented!');
-
-
 /* eslint-disable react/prefer-stateless-function */
-class HomePage extends React.Component {
+class MapPage extends React.Component {
   render() {
     return (
       <div>
-        <Map
-          buildings={objects}
-          hideInfos={mockFnc}
-        />
+        building map
       </div>
     );
   }
 }
 
-HomePage.propTypes = {
+MapPage.propTypes = {
 };
-export default HomePage;
+export default MapPage;
