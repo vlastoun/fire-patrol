16a8f86560be254cab6ed49aadf5ecd0b10ff3bc
Vlastik <vlastoun@gmail.com>
2017-08-28 21:53:21 +0200

    test data

--- src/data/index.js
+++ src/data/index.js
@@ -16,254 +16,259 @@ export const groups = [
   },
 ];
 
-export const objects = [{
-  "id": 1,
-  "label": 1,
-  "top": 1301,
-  "left": 1723
+export const objects = [
+  OBJ_01_PILA,
+  OBJ_02_VYKLADKA,
+];
+
+export const mockobjects = [{
+  id: 1,
+  label: 1,
+  top: 1301,
+  left: 1723
 }, {
-  "id": 2,
-  "label": 2,
-  "top": 851,
-  "left": 1007
+  id: 2,
+  label: 2,
+  top: 851,
+  left: 1007
 }, {
-  "id": 3,
-  "label": 3,
-  "top": 647,
-  "left": 2101
+  id: 3,
+  label: 3,
+  top: 647,
+  left: 2101
 }, {
-  "id": 4,
-  "label": 4,
-  "top": 1484,
-  "left": 1214
+  id: 4,
+  label: 4,
+  top: 1484,
+  left: 1214
 }, {
-  "id": 5,
-  "label": 5,
-  "top": 209,
-  "left": 1301
+  id: 5,
+  label: 5,
+  top: 209,
+  left: 1301
 }, {
-  "id": 6,
-  "label": 6,
-  "top": 829,
-  "left": 2417
+  id: 6,
+  label: 6,
+  top: 829,
+  left: 2417
 }, {
-  "id": 7,
-  "label": 7,
-  "top": 925,
-  "left": 62
+  id: 7,
+  label: 7,
+  top: 925,
+  left: 62
 }, {
-  "id": 8,
-  "label": 8,
-  "top": 10,
-  "left": 1013
+  id: 8,
+  label: 8,
+  top: 10,
+  left: 1013
 }, {
-  "id": 9,
-  "label": 9,
-  "top": 1342,
-  "left": 2317
+  id: 9,
+  label: 9,
+  top: 1342,
+  left: 2317
 }, {
-  "id": 10,
-  "label": 10,
-  "top": 1015,
-  "left": 2299
+  id: 10,
+  label: 10,
+  top: 1015,
+  left: 2299
 }, {
-  "id": 11,
-  "label": 11,
-  "top": 1319,
-  "left": 354
+  id: 11,
+  label: 11,
+  top: 1319,
+  left: 354
 }, {
-  "id": 12,
-  "label": 12,
-  "top": 1205,
-  "left": 2567
+  id: 12,
+  label: 12,
+  top: 1205,
+  left: 2567
 }, {
-  "id": 13,
-  "label": 13,
-  "top": 1029,
-  "left": 1252
+  id: 13,
+  label: 13,
+  top: 1029,
+  left: 1252
 }, {
-  "id": 14,
-  "label": 14,
-  "top": 1208,
-  "left": 1052
+  id: 14,
+  label: 14,
+  top: 1208,
+  left: 1052
 }, {
-  "id": 15,
-  "label": 15,
-  "top": 207,
-  "left": 1841
+  id: 15,
+  label: 15,
+  top: 207,
+  left: 1841
 }, {
-  "id": 16,
-  "label": 16,
-  "top": 831,
-  "left": 155
+  id: 16,
+  label: 16,
+  top: 831,
+  left: 155
 }, {
-  "id": 17,
-  "label": 17,
-  "top": 1204,
-  "left": 1133
+  id: 17,
+  label: 17,
+  top: 1204,
+  left: 1133
 }, {
-  "id": 18,
-  "label": 18,
-  "top": 443,
-  "left": 17
+  id: 18,
+  label: 18,
+  top: 443,
+  left: 17
 }, {
-  "id": 19,
-  "label": 19,
-  "top": 1533,
-  "left": 1136
+  id: 19,
+  label: 19,
+  top: 1533,
+  left: 1136
 }, {
-  "id": 20,
-  "label": 20,
-  "top": 1209,
-  "left": 2053
+  id: 20,
+  label: 20,
+  top: 1209,
+  left: 2053
 }, {
-  "id": 21,
-  "label": 21,
-  "top": 1254,
-  "left": 1233
+  id: 21,
+  label: 21,
+  top: 1254,
+  left: 1233
 }, {
-  "id": 22,
-  "label": 22,
-  "top": 1249,
-  "left": 2151
+  id: 22,
+  label: 22,
+  top: 1249,
+  left: 2151
 }, {
-  "id": 23,
-  "label": 23,
-  "top": 520,
-  "left": 799
+  id: 23,
+  label: 23,
+  top: 520,
+  left: 799
 }, {
-  "id": 24,
-  "label": 24,
-  "top": 1456,
-  "left": 1133
+  id: 24,
+  label: 24,
+  top: 1456,
+  left: 1133
 }, {
-  "id": 25,
-  "label": 25,
-  "top": 331,
-  "left": 1731
+  id: 25,
+  label: 25,
+  top: 331,
+  left: 1731
 }, {
-  "id": 26,
-  "label": 26,
-  "top": 722,
-  "left": 644
+  id: 26,
+  label: 26,
+  top: 722,
+  left: 644
 }, {
-  "id": 27,
-  "label": 27,
-  "top": 388,
-  "left": 1203
+  id: 27,
+  label: 27,
+  top: 388,
+  left: 1203
 }, {
-  "id": 28,
-  "label": 28,
-  "top": 905,
-  "left": 1711
+  id: 28,
+  label: 28,
+  top: 905,
+  left: 1711
 }, {
-  "id": 29,
-  "label": 29,
-  "top": 42,
-  "left": 1441
+  id: 29,
+  label: 29,
+  top: 42,
+  left: 1441
 }, {
-  "id": 30,
-  "label": 30,
-  "top": 1296,
-  "left": 2196
+  id: 30,
+  label: 30,
+  top: 1296,
+  left: 2196
 }, {
-  "id": 31,
-  "label": 31,
-  "top": 205,
-  "left": 186
+  id: 31,
+  label: 31,
+  top: 205,
+  left: 186
 }, {
-  "id": 32,
-  "label": 32,
-  "top": 939,
-  "left": 1471
+  id: 32,
+  label: 32,
+  top: 939,
+  left: 1471
 }, {
-  "id": 33,
-  "label": 33,
-  "top": 1243,
-  "left": 403
+  id: 33,
+  label: 33,
+  top: 1243,
+  left: 403
 }, {
-  "id": 34,
-  "label": 34,
-  "top": 680,
-  "left": 635
+  id: 34,
+  label: 34,
+  top: 680,
+  left: 635
 }, {
-  "id": 35,
-  "label": 35,
-  "top": 1694,
-  "left": 1985
+  id: 35,
+  label: 35,
+  top: 1694,
+  left: 1985
 }, {
-  "id": 36,
-  "label": 36,
-  "top": 555,
-  "left": 2483
+  id: 36,
+  label: 36,
+  top: 555,
+  left: 2483
 }, {
-  "id": 37,
-  "label": 37,
-  "top": 696,
-  "left": 1268
+  id: 37,
+  label: 37,
+  top: 696,
+  left: 1268
 }, {
-  "id": 38,
-  "label": 38,
-  "top": 906,
-  "left": 1404
+  id: 38,
+  label: 38,
+  top: 906,
+  left: 1404
 }, {
-  "id": 39,
-  "label": 39,
-  "top": 1196,
-  "left": 1141
+  id: 39,
+  label: 39,
+  top: 1196,
+  left: 1141
 }, {
-  "id": 40,
-  "label": 40,
-  "top": 1225,
-  "left": 563
+  id: 40,
+  label: 40,
+  top: 1225,
+  left: 563
 }, {
-  "id": 41,
-  "label": 41,
-  "top": 1462,
-  "left": 354
+  id: 41,
+  label: 41,
+  top: 1462,
+  left: 354
 }, {
-  "id": 42,
-  "label": 42,
-  "top": 1149,
-  "left": 1489
+  id: 42,
+  label: 42,
+  top: 1149,
+  left: 1489
 }, {
-  "id": 43,
-  "label": 43,
-  "top": 1184,
-  "left": 1050
+  id: 43,
+  label: 43,
+  top: 1184,
+  left: 1050
 }, {
-  "id": 44,
-  "label": 44,
-  "top": 1231,
-  "left": 1596
+  id: 44,
+  label: 44,
+  top: 1231,
+  left: 1596
 }, {
-  "id": 45,
-  "label": 45,
-  "top": 1278,
-  "left": 1329
+  id: 45,
+  label: 45,
+  top: 1278,
+  left: 1329
 }, {
-  "id": 46,
-  "label": 46,
-  "top": 1219,
-  "left": 512
+  id: 46,
+  label: 46,
+  top: 1219,
+  left: 512
 }, {
-  "id": 47,
-  "label": 47,
-  "top": 1545,
-  "left": 1919
+  id: 47,
+  label: 47,
+  top: 1545,
+  left: 1919
 }, {
-  "id": 48,
-  "label": 48,
-  "top": 1007,
-  "left": 206
+  id: 48,
+  label: 48,
+  top: 1007,
+  left: 206
 }, {
-  "id": 49,
-  "label": 49,
-  "top": 209,
-  "left": 1483
+  id: 49,
+  label: 49,
+  top: 209,
+  left: 1483
 }, {
-  "id": 50,
-  "label": 50,
-  "top": 1444,
-  "left": 1984
-}]
+  id: 50,
+  label: 50,
+  top: 1444,
+  left: 1984
+}];
